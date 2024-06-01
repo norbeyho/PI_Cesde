b685ca2705d60aab6f9448cd1b032cb5148849be
norbeyho <norbeyho@hotmail.com>
2024-05-31 20:02:18 -0500

    v7

--- Home.py
+++ Home.py
@@ -6,4 +6,4 @@ st.write("Elizabeth Restrepo")
 st.write("Richard Blanco")
 st.write("Norbey Hernandez")
 
-st.image("img\fondo.jpeg", caption="Disfruta de nuestro contenido")
+st.image("img/fondo.jpeg", caption="Disfruta de nuestro contenido")
