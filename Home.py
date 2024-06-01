e9df7c1f2bceb9274fc6e5efe3d6be2ec17047a5
norbeyho <norbeyho@hotmail.com>
2024-05-31 19:59:06 -0500

    v6

--- Home.py
+++ Home.py
@@ -5,3 +5,5 @@ st.title("Proyecto Integrador Python")
 st.write("Elizabeth Restrepo")
 st.write("Richard Blanco")
 st.write("Norbey Hernandez")
+
+st.image("img\fondo.jpeg", caption="Disfruta de nuestro contenido")
