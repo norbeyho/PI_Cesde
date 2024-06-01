ddfe707bc1663d26ca2832086b8ea92586eb2a7d
norbeyho <norbeyho@hotmail.com>
2024-05-31 20:08:22 -0500

    v8

--- Home.py
+++ Home.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.title("Proyecto Integrador Python")
 
+st.image("img/fondo.jpeg", caption="Disfruta de nuestro contenido")
+
 st.write("Elizabeth Restrepo")
 st.write("Richard Blanco")
 st.write("Norbey Hernandez")
-
-st.image("img/fondo.jpeg", caption="Disfruta de nuestro contenido")
